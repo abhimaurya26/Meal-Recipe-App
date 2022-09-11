7259a3fa62c2342a2d8c75b5d9a9ab5f6a0cd9cc
abhimaurya26 <82035539+abhimaurya26@users.noreply.github.com>
2022-09-12 01:25:41 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# Meal Recipe App - Front End Development Project
+
+Used technologies: HTML, CSS, Javascript, ReactJS.
+Used API: TheMealDB API.
+
+This app can be used to get recipe of any desired food item.
+Favorite recipes can be stored/removed in favourite container.
+
---
+++ src/components/Search.js
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import { useGlobalContext } from '../context'
+
+
+
+const Search = () => {
+  const { setSearchTerm, fetchRandomMeal } = useGlobalContext()
+  const [text, setText] = useState('')
+
+  const handleChange = (e) => {
+    setText(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (text) {
+      setSearchTerm(text)
+    }
+    setText('')
+  }
+
+  const handleRandomMeal = () => {
+    setSearchTerm('')
+    setText('')
+    fetchRandomMeal()
+  }
+
+  return <header className='search-container'>
+    <form onSubmit={handleSubmit} >
+      <input type='text' onChange={handleChange} value={text} placeholder='type favorite meal' className='form-input' />
+      <button type="submit" className="btn">search</button>
+      <button type="btn" className="btn btn-random" onClick={handleRandomMeal}>suprise me !</button>
+    </form>
+    <div className='app-name'>
+      <h3 >Meal Recipe App</h3>
+    </div>
+  </header>
+}
+
+
+export default Search
